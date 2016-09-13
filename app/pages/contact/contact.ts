a1c98bce810eb3e34f03f1cf966313a61270b9c9
Dmitry Slepchenko <slepchenko.dima@gmail.com>
2016-09-13 10:05:20 +0300

    added take picture example code

--- app/pages/contact/contact.ts
+++ app/pages/contact/contact.ts
@@ -1,10 +1,28 @@
 import {Component} from '@angular/core';
 import {NavController} from 'ionic-angular';
+import {Camera} from 'ionic-native';
+
 
 @Component({
   templateUrl: 'build/pages/contact/contact.html'
 })
 export class ContactPage {
+  private base64Image:string;
+  errorMessage:string;
   constructor(private navCtrl: NavController) {
+  }
+  takePicture(){
+    Camera.getPicture({
+      mediaType: Camera.MediaType.PICTURE,
+      destinationType: Camera.DestinationType.DATA_URL,
+      targetWidth: 1000,
+      targetHeight: 1000
+    }).then((imageData) => {
+      // imageData is a base64 encoded string
+      this.base64Image = "data:image/jpeg;base64," + imageData;
+    }, (err) => {
+      console.log(err);
+      this.errorMessage = err;
+    });
   }
 }
